5a8fe017aa77e7a613347f6494375c6ab777dead
cmulhern <collin.mulhern@gmail.com>
2012-11-05 11:10:28 -0800

    Added columns to events and changed the form to make them.

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121105105227) do
+ActiveRecord::Schema.define(:version => 20121105185949) do
 
   create_table "events", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.string   "name"
     t.integer  "userid"
+    t.string   "location"
+    t.time     "start_time"
+    t.time     "end_time"
+    t.date     "start_date"
+    t.date     "end_date"
   end
 
   create_table "objectives", :force => true do |t|
