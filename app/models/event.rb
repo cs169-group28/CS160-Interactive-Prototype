8133b5d972c6a55d103cdb5483e8368ae1a4d894
saasbook <saasbook@saasbook.(none)>
2012-12-03 16:03:16 -0800

    replaced event modal with bootstrap and got rid of extra javascripts

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ActiveRecord::Base
   # attr_accessible :title, :body
   attr_accessible :name, :userid, :start_date, :start_time, :end_date, :end_time, :location
-  has_many :tasks
+  has_many :tasks, :dependent => :destroy
   has_many :objectives, :through => :tasks
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,5 @@ class Task < ActiveRecord::Base
   # attr_accessible :title, :bod 
   attr_accessible :event_id, :b_flag, :budget, :owner, :name, :description, :completed
   belongs_to :event
-  has_many :objectives
+  has_many :objectives, :dependent => :destroy
 end
